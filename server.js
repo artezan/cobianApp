61d4fe3c50fbd45d06651db26ab5c159052c56ce
artezan <artezan_015@hotmail.com>
2018-10-08 17:58:13 -0500

    server fix

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //Install express server
 const express = require('express');
+var compression = require('compression')
 const path = require('path');
 
 const app = express();
-
+app.use(compression())
 // Serve only the static files form the dist directory
 app.use(express.static('./www/'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/www/index.html'));
 });
 
